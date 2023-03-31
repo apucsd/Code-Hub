5ab279c5d09ef1ffc4501b6de30e5509afd8fc7b
Apu Sutra Dhar <apusutradhar77@gmail.com>
2023-03-31 10:41:11 +0600

    added for duplicate bookmark item

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react";
 import Card from "../Card/Card";
 import SideCart from "../SideCart/SideCart";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const Main = () => {
   const [data, setData] = useState([]);
@@ -29,9 +31,14 @@ const Main = () => {
     let bookmark = [];
     const prevBookmark = JSON.parse(localStorage.getItem("bookmark"));
     if (prevBookmark) {
-      bookmark.push(...prevBookmark, blogTitle);
+      const allReadyBookMark = prevBookmark.find((title) => title == blogTitle);
+      if (allReadyBookMark) {
+        toast("You already bookmarked this...");
+      } else {
+        bookmark.push(...prevBookmark, blogTitle);
 
-      localStorage.setItem("bookmark", JSON.stringify(bookmark));
+        localStorage.setItem("bookmark", JSON.stringify(bookmark));
+      }
     } else {
       bookmark.push(blogTitle);
       localStorage.setItem("bookmark", JSON.stringify(bookmark));
@@ -55,6 +62,7 @@ const Main = () => {
       <div className=" ">
         <SideCart handleTitle={handleTitle} spendTime={spendTime}></SideCart>
       </div>
+      <ToastContainer />
     </div>
   );
 };
